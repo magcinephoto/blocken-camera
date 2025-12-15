1eab4cafd564a4bf52db00e596764df516670b3d
Mag <mag.cinephoto@gmail.com>
2025-12-15 21:25:54 +0900

    feat: add app_id

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export async function generateMetadata(): Promise<Metadata> {
       "fc:frame": JSON.stringify({
         version: minikitConfig.miniapp.version,
         imageUrl: minikitConfig.miniapp.heroImageUrl,
+        'base:app_id': '693ffa89d77c069a945bdf06',
         button: {
           title: `Join the ${minikitConfig.miniapp.name} Waitlist`,
           action: {
