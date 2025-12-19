c34b78d789c14596f2615ccf1b95b105799775dc
Mag <mag.cinephoto@gmail.com>
2025-12-19 22:27:58 +0900

    fix: remove

--- app/components/WalletConnect.tsx
+++ app/components/WalletConnect.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import { ConnectWallet } from "@coinbase/onchainkit/wallet";
-import { useMiniKit } from "@coinbase/onchainkit/minikit";
-import { useAccount } from "wagmi";
 import styles from "./WalletConnect.module.css";
 
 export function WalletConnect() {
